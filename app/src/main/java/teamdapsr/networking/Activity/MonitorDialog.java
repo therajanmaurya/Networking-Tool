cbeb3328151cae980a458266948339a9d7ae07c2
therajanmaurya <rajanmaurya154@gmail.com>
2015-11-16 17:41:44 +0530

    Monitor service response done

--- app/src/main/java/teamdapsr/networking/Activity/MonitorDialog.java
+++ app/src/main/java/teamdapsr/networking/Activity/MonitorDialog.java
@@ -1,11 +1,13 @@
 package teamdapsr.networking.Activity;
 
 import android.content.Context;
+import android.os.Handler;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import teamdapsr.networking.R;
 import teamdapsr.networking.connectionInfo.ConnectionDetector;
+import teamdapsr.networking.ping.Ping;
 
 /**
  * Created by RajanMaurya on 16/11/15.
@@ -14,26 +16,39 @@ public class MonitorDialog {
 
     Context  context;
     ConnectionDetector connectionDetector;
-    TextView networktype;
+    TextView networktype , mServiceresponse;
+    Process process;
+    Runtime runtime;
+    private Handler handler;
 
     public MonitorDialog(Context context)
     {
         this.context = context;
         connectionDetector = new ConnectionDetector(context);
+        handler = new Handler();
+        runtime = Runtime.getRuntime();
 
     }
 
-    public void startDialog(String title )
+    public void startDialog(String host_data )
     {
 
+
+
         MaterialDialog dialog = new MaterialDialog.Builder(context)
-                .title("Check: " +title)
+                .title("Check: " + host_data)
                 .customView(R.layout.monitordialog, true)
                 .negativeText("OK")
                 .build();
 
         networktype = (TextView) dialog.getCustomView().findViewById(R.id.connectiontype);
         networktype.setText("Network Connection :" +connectionDetector.ConnectionType());
+
+        mServiceresponse = (TextView) dialog.getCustomView().findViewById(R.id.serviceresponse);
+
+        Ping ping = new Ping(context, process, runtime, host_data, mServiceresponse, handler);
+        Thread thread = new Thread(ping);
+        thread.start();
 
         dialog.show();
 
